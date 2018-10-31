898069fac7fc6305f4637987941b06667577d5ec
mikesy <michaelyoung.aero@gmail.com>
2018-10-31 15:42:12 -0500

    add capability to cycle through multiple bags with checks and closing of bags

--- rosbag_processing/bags2array.py
+++ rosbag_processing/bags2array.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+
+# if you feed multiple bags into the bag object, it will combine them using their start times
+# it assumes you put the bags in order starting from the first (earliest) bag
+
 import rosbag
 import yaml
 import numpy as np
@@ -7,34 +11,50 @@ import operator      #makes it easier to pull sub-topics using attrgetter
 
 
 class bagObject:
-    def __init__(self,bag_name,topics, subtopics,save_prefix):
-        self.bag = rosbag.Bag(bag_name)
+    def __init__(self,bag_names,topics, subtopics,save_prefix):
+
+        # initialize 
+        self.bags = self.bags_into_array(bag_names)
         self.topics = topics
         self.subtopics = subtopics
         self.save_prefix = save_prefix
-        self.start_time = 0
-        self.topic_lengths = {}#np.zeros(len(topics))
-        self.get_bag_info_clean()
-        
-        for topic in topics:
-            self.print_topic_example(topic)
-        index = 0
-        for (topic, msg, t) in self.bag.read_messages(topics=['/task_status']):
-            index += 1
-        print(index)
-        print(self.topic_lengths)
-        self.topic_into_nparray()
-        self.bag.close()
+        self.start_times = []
+        self.info_dicts = []
+        self.topic_lengths = []
 
+        #do stuff
+        self.bags_into_array(bag_names)
+        self.preprocess()
+        self.convert_topics_to_nparray()
+
+    def __del__(self):  # close the bags of destruction
+        self.close_bags()
+
+    ## main functions that processes all the topics
+    def bags_into_array(self,bag_names):
+        bags = []
+        for bag_name in bag_names:
+            bags.append(rosbag.Bag(bag_name))
+        return bags
+
+    def preprocess(self): # all info necessary to cycle through all topics 
+        for bag in self.bags:
+            info_dict = yaml.load(bag._get_yaml_info())
+            self.info_dicts.append(info_dict)
+            topic_length_dict = {}
+            for item in info_dict['topics']:
+                #if it's a topic I care about, tell me how many messages I have for np use
+                if item['topic'] in self.topics:
+                    topic_length_dict[item['topic']] = item['messages']
+            self.topic_lengths.append(topic_length_dict)
+            self.start_times.append(info_dict['start'])
+        self.start_time_check()
+
+
+    ## functions to provide user with info about the bag, to be called in separate script
     def get_bag_info_clean(self):
-        info_dict = yaml.load(self.bag._get_yaml_info())
-        self.start_time = info_dict['start']
-
         print("Printing information about topics with more than 1 messages")
-        for item in info_dict['topics']:
-            #if it's a topic I care about, tell me how many messages I have for np use
-            if item['topic'] in self.topics:
-                self.topic_lengths[item['topic']] = item['messages'] 
+        for item in self.info_dicts[0]['topics']:
             if (item['messages'] > 1): 
                 print("------------")
                 print("topic name: " + item['topic'] + " \t type: " + item['type'])
@@ -44,30 +64,43 @@ class bagObject:
     def print_topic_example(self, topic_name):
         print("------------")
         print("An example message of topic: " + topic_name)
-        for (topic, msg, t) in self.bag.read_messages(topics=[topic_name]):
+        for (topic, msg, t) in self.bags[0].read_messages(topics=[topic_name]):
             print(msg)
             break
         print("------------")
 
-    def topic_into_nparray(self):
+    def convert_topics_to_nparray(self):
 
         for topic_index, topic in enumerate(self.topics):
-
-            t_array = np.zeros(self.topic_lengths[topic])
-            data_array = np.empty([len(self.subtopics[topic_index]), self.topic_lengths[topic]],dtype=object)
+            total_topic_length = self.get_total_topic_length(topic)
+            t_array = np.zeros(total_topic_length)
+            data_array = np.empty([len(self.subtopics[topic_index]), total_topic_length],dtype=object)
             time_index = 0
-            for (unused,msg,t) in self.bag.read_messages(topics=[topic]):
-                t_array[time_index] = t.secs + t.nsecs*1e-9
-                for subtopic_index, subtopic in enumerate(self.subtopics[topic_index]):
-                    data_array[subtopic_index,time_index] = operator.attrgetter(subtopic)(msg)
-                    
-                time_index += 1
+            for bag in self.bags:
+                for (unused,msg,t) in bag.read_messages(topics=[topic]):
+                    t_array[time_index] = t.secs + t.nsecs*1e-9
+                    for subtopic_index, subtopic in enumerate(self.subtopics[topic_index]):
+                        data_array[subtopic_index,time_index] = operator.attrgetter(subtopic)(msg)
+                        
+                    time_index += 1
         #save...
             filename = self.save_prefix + '_' + self.topics[topic_index][1:]   # skip the '/' at beginning of topic name
             np.save(filename, data_array)
             filename += '_time'
             np.save(filename, t_array)
-            print(t_array)
-            print(data_array)
 
-
+    def get_total_topic_length(self, topic):
+        topic_length_total = 0
+        for topic_length_dict in self.topic_lengths:
+            topic_length_total += topic_length_dict[topic]
+            print(topic_length_total)
+        return topic_length_total
+    ## checks and misc
+    def start_time_check(self):
+        for st in self.start_times[1:]:
+            if st < self.start_times[0]:
+                raise ValueError("The bags you passed are out of order")
+    
+    def close_bags(self):
+        for bag in self.bags:
+            bag.close()
